51f62bd61ff23547329323dcc9be70cfb7069fca
rami-majdoub <rami.majdoub1@gmail.com>
2022-07-21 23:23:54 +0100

    withdraw not working

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -5,30 +5,24 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+	
+	mapping(address => uint) public balanceOf;
+	
+	receive () external payable {
+		balanceOf[msg.sender] += msg.value;
+	}
+	
+	function withdraw() public {
+		require(balanceOf[msg.sender] > 0);
+		
+		console.log("1-", msg.sender.balance);
+		uint val = balanceOf[msg.sender];
 
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
-    }
-
-    function withdraw() public {
-        // Uncomment this line to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
-    }
+		address payable sender = payable(msg.sender);
+		(bool success, ) = sender.call{value: val}("");
+		require(success);
+		console.log("2-", msg.sender.balance);
+		
+		balanceOf[msg.sender] = 0;
+	}
 }
